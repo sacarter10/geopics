02f228b30670dc98cc36322e8256b17766f7309a
Sally Carter <sacarter10@gmail.com>
2013-10-12 09:56:14 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GeoPics::Application.config.secret_key_base = 'fb8e8f0b21f76b6ef6d759e7592b16eb90afe0370888118af1f68935cebded11e63731336137cbf7fe1dfb93d319d7ce6efcb61f1e904dac6a695577b36290f9'
